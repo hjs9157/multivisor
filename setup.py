1546c76353e1a1aebb4cc6e2aef2d0366c96f836
Joonseok Hur <hjs9157@gmail.com>
2025-03-01 15:39:50 -0700

    Added favicon

--- setup.py
+++ setup.py
@@ -27,7 +27,12 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(),
     package_data={
-        "multivisor.server": ["dist/*", "dist/static/css/*", "dist/static/js/*"]
+        "multivisor.server": [
+            "dist/*",
+            "dist/static/css/*",
+            "dist/static/js/*",
+            "dist/static/favicon.ico",  # Ensure favicon is included
+        ]
     },
     entry_points=dict(
         console_scripts=[
